1efc1ee7b6b2304b212b4540672b9ad19737f849
nagetpresto <122101296+nagetpresto@users.noreply.github.com>
2024-08-11 04:08:21 +0700

    Update nac-table.js

--- nac-table.js
+++ nac-table.js
@@ -732,37 +732,7 @@ let AndysTable = _decorate([e$1('andys-table')], function (_initialize, _LitElem
         }}"
                   class="table-row ${this.editMode && this.selectedRow === item ? "edit" : ""} ${this.selectedRow === item ? "selected" : ""}"
                 >
-		<td>
-                	${this.editMode && this.selectedRow === item ? y`<input
-                              type="text"
-                              .value="${item[column.field]}"
-                              class="table-cell-input input-styled"
-                              @input="${event => this.onCellEdit({
-			          field: checkbox,
-			          value: event.target.value
-			        })}"
-                              @focus="${() => {
-			          this.editCell = {
-			            columnName: checkbox,
-			            row: item
-			          };
-			          this.requestUpdate();
-			        }}"
-                              @blur="${() => {
-			          if (this.editCell?.row === item) {
-			            this.editCell = {
-			              columnName: checkbox,
-			              row: item
-			            };
-			            this.requestUpdate();
-			          }
-			        }}"
-	   
-                              ? disabled=${!!this.readonly}
-                            />` : y`<span class="table-cell-value"
-                              >${item[checkbox]}</span
-                            >`}
-                      </td>
+		
 
    
                   ${this.columns.map(column => y`
