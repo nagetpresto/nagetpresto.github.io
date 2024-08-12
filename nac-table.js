15035a3923891d12f52f584fe613c3b6f760192a
bilqistwork <bilqistaz27@gmail.com>
2024-08-12 19:52:05 +0700

    change display name

--- nac-table.js
+++ nac-table.js
@@ -549,14 +549,15 @@ let AndysTable = _decorate([e$1('andys-table')], function (_initialize, _LitElem
 	        return {Action: false, ...row };
 	      });
 
-      this.orderMapping = this.colMapping.reduce((acc, curr) => {
-        acc[curr.Title] = {
-          order: parseInt(curr.Order0, 10),
-          dataType: curr.DataType,
-          displayName: curr.DisplayName
-        };
-        return acc;
+        this.orderMapping = this.colMapping.reduce((acc, curr) => {
+          acc[curr.Title] = {
+              order: parseInt(curr.Order0, 10),
+              dataType: curr.DataType,
+              displayName: curr.DisplayName
+          };
+          return acc;
       }, {});
+      //console.log(this.orderMapping, "orderMapping");
       
       const formatData = (value, dataType) => {
           switch (dataType) {
@@ -584,20 +585,20 @@ let AndysTable = _decorate([e$1('andys-table')], function (_initialize, _LitElem
                   return aOrder - bOrder;
               })
               .forEach(key => {
-                  if (this.orderMapping[key]) {
-                    this.formattedValue = formatData(item[key], this.orderMapping[key].dataType);
-                    this.displayName = this.orderMapping[key].displayName;
-                    this.orderedItem[this.displayName] = this.formattedValue;
+                  const mappingKey = Object.keys(this.orderMapping).find(k => k === key);
+                  if (mappingKey) {
+                      this.formattedValue = formatData(item[key], this.orderMapping[mappingKey].dataType);
+                      this.displayName = this.orderMapping[mappingKey].displayName;
+                      this.orderedItem[this.displayName] = this.formattedValue;
                   } else {
-                    this.orderedItem[key] = item[key];
+                      this.orderedItem[key] = item[key];
                   }
               });
           return this.orderedItem;
       });
       
-      //console.log(this.orderedData, "orderData");
-      //this.data = this.orderedData;
-
+      console.log(this.data, "processedData");
+      
       this.updatePageData();
 
           } catch (e) {
