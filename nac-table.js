928cf684b415decb3de4f5bf29f1555aab35163d
nagetpresto <122101296+nagetpresto@users.noreply.github.com>
2024-09-11 09:59:19 +0700

    Update nac-table.js

--- nac-table.js
+++ nac-table.js
@@ -853,7 +853,8 @@ let AndysTable = _decorate([e$1('andys-table')], function (_initialize, _LitElem
             position: relative;
             padding: 26px;
         ">
-            <input id="select-all" type="checkbox" class="table-cell-input input-styled" ${this.isapproval ? "disabled" : ""}>    
+            ${this.isapproval ? y`<input id="select-all" type="checkbox" class="table-cell-input input-styled" disabled>` : y`<input id="select-all" type="checkbox" class="table-cell-input input-styled">`}   
+            
             </div>
             <div>
                 <p>Select All</p>
